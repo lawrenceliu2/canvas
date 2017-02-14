085ca7be592002993e23bda0be3f32faab97efdc
Lawrence Liu <lawrence255@yahoo.com>
2017-02-14 13:07:37 -0500

    sorta works?

--- canvas.js
+++ canvas.js
@@ -1,7 +1,7 @@
 var c = document.getElementById("slate")
 var ctx = c.getContext("2d");
 var b = document.getElementById("b")
-
+ctx.fillStyle = "#FF00FF";
 
 var clearCanvas = function() {
     ctx.clearRect(0, 0, 500, 500)
@@ -9,14 +9,24 @@ var clearCanvas = function() {
 
 b.addEventListener("click", clearCanvas)
 
-
-ctx.fillStyle = "#0000FF";
-ctx.fillRect(50,50,100,200);
-
 var drawRectangle = function() {
     var x = event.clientX;
     var y = event.clientY;
-    ctx.fillRect(x,y,100,150)
+    ctx.fillRect(x - 10, y - 115,100,150)
 };
 
-c.addEventListener("click", drawRectangle)
+//c.addEventListener("click", drawRectangle)
+
+ctx.beginPath();
+
+var drawCircle = function() {
+    //ctx.moveTo(x - 10, y - 115);
+    var x = event.clientX;
+    var y = event.clientY;
+    ctx.arc(x - 10, y - 115,10,0,2*Math.PI);
+    ctx.fill();
+    ctx.stroke();
+    ctx.moveTo(x - 10, y - 115);
+};
+
+c.addEventListener("click", drawCircle)
