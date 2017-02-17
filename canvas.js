1d9469bec3d7c2f469c977286e8af5d01c9ead17
Lawrence Liu <lawrence255@yahoo.com>
2017-02-17 13:06:45 -0500

    only the upper ceiling of the canvas has issues with bouncing, it starts bouncing down too early

--- canvas.js
+++ canvas.js
@@ -1,14 +1,19 @@
 var c = document.getElementById("slate")
 var ctx = c.getContext("2d");
 var b = document.getElementById("b")
+var circle = document.getElementById("c")
+var s = document.getElementById("stop")
+var dvd = document.getElementById("dvd")
 ctx.fillStyle = "#FF00FF";
 
+//Clear the canvas
 var clearCanvas = function() {
     ctx.clearRect(0, 0, c.width, c.height)
 };
 
-//b.addEventListener("click", clearCanvas);
+b.addEventListener("click", clearCanvas);
 
+//Draw a rectangle at click location
 var drawRectangle = function() {
     var x = event.clientX;
     var y = event.clientY;
@@ -19,6 +24,7 @@ var drawRectangle = function() {
 
 ctx.beginPath();
 
+//Draw a circle at click location
 var drawCircle = function() {
     var x = event.clientX;
     var y = event.clientY;
@@ -30,9 +36,18 @@ var drawCircle = function() {
 
 //c.addEventListener("click", drawCircle);
 
+//Stops all animation
+var stop = function(){
+    console.log(requestID);
+    window.cancelAnimationFrame(requestID);
+};
+
+s.addEventListener("click", stop);
+
 var requestID;
 
-var animate = function(){
+//Moves a dot across the screen horizontally
+var animateDot = function(){
     window.cancelAnimationFrame(requestID);
     var xcor = 0;
 
@@ -50,15 +65,87 @@ var animate = function(){
 	}
 	requestID = window.requestAnimationFrame(dotStuff);
     };
-
     dotStuff();
 };
 
-var stop = function(){
-    console.log(requestID);
-    window.cancelAnimationFrame(requestID);
+c.addEventListener("click", animateDot);
+
+//Animates a circle at the center of the canvas that increases/decreases in radius continuously
+var animateCircle = function(){
+    window.cancelAnimationFrame(requestID); 
+    var radius = 1;
+    var walled = false;
+
+    var circleStuff = function(){
+	console.log(requestID);
+	ctx.clearRect(0,0,c.width,c.height);
+	ctx.beginPath()
+	ctx.arc(250,250,radius,0,360);
+	ctx.fill();
+	ctx.stroke();
+	if (radius==250){
+	    walled = true;
+	}
+	if (radius==1){
+	    walled = false;
+	}
+	if (walled){
+	    radius--;
+	}else{
+	    radius++;
+	}
+
+	requestID = window.requestAnimationFrame(circleStuff);
+    };
+    circleStuff();
 };
 
-b.addEventListener("click", stop);
+circle.addEventListener("click", animateCircle);
 
-c.addEventListener("click", animate);
+//Bounces around a DVD image
+var animateDVD = function(){
+    window.cancelAnimationFrame(requestID); 
+    var xcor = ((Math.random() * 300) + 100);
+    var ycor = ((Math.random() * 300) + 100);
+    var walled = false
+    var floored = false 
+    var pic = new Image();
+    pic.src = "http://plainicon.com/download-icons/46993/plainicon.com-46993-ec84-128px.png";
+
+    var dvdStuff = function(){
+	console.log(ycor);
+
+	ctx.clearRect(0,0,c.width,c.height);
+	ctx.beginPath();
+	ctx.drawImage(pic,xcor,ycor);
+	ctx.fill();
+	ctx.stroke();
+	if (xcor >= 370){
+	    walled = true;
+	}
+	if (xcor <= 0){
+	    walled = false;
+	}
+	if (ycor >= 415){
+	    floored = true;
+	}
+	if (ycor <= 1){
+	    floored = false;
+	}  
+	if (walled){
+	    xcor--;
+	}else{
+	    xcor++;
+	}
+	if (floored){
+	    ycor--;
+	}else{
+	    ycor++;
+	}
+	
+	requestID = window.requestAnimationFrame(dvdStuff);
+    };
+    dvdStuff();
+};
+
+dvd.addEventListener("click", animateDVD);
